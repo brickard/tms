410eef3929260e94541acc883542ad6932ea59b1
Bram Swenson <bram@craniumisajar.com>
2010-09-21 22:25:18 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storeresetter::Application.config.secret_token = '16a17746060ed15bf7266eed98c9d11d66942df7a3ad487b192387c086b05f28c3e356e8d0089d1ff203044b53713f8923de102d29c053dfbbcbcec303636925'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Storeresetter::Application.config.session_store :cookie_store, :key => '_storeresetter_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Storeresetter::Application.config.session_store :active_record_store
