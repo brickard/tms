5d1f9075a49a57a3f0757b589ffe46a0f3e119a4
Bram Swenson <bram@craniumisajar.com>
2010-09-23 19:59:14 -0500

    fixed missing comma in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me
