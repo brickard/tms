1fbab3322586e8dc1ced00cd4fc41f7bc580c07c
Bram Swenson <bram@craniumisajar.com>
2010-09-26 14:24:05 -0500

    added people to navigation menu

--- config/navigation.rb
+++ config/navigation.rb
@@ -23,6 +23,7 @@ SimpleNavigation::Configuration.run do |navigation|
 
   # Define the primary navigation
   navigation.items do |primary|
+    primary.item :people,   'People',   people_path
     primary.item :clients,  'Clients',  clients_path
     primary.item :regions,  'Regions',  regions_path
     primary.item :stores,   'Stores',   stores_path
