1fa7fc8d95511fd4cf4827ff071f543a13ae44b2
Bram Swenson <bram@craniumisajar.com>
2010-09-26 12:55:46 -0500

    updated mailer settings for all environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,7 @@ Storeresetter::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # setup mailer
+  config.action_mailer.default_url_options = { :host => 'storeresetter.heroku.com' }
 end
--- config/initializers/mailer.rb
+++
@@ -1,1 +0,0 @@
-Storeresetter::Application.config.action_mailer.default_url_options = { :host => 'localhost:3000' }
