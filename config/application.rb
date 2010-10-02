cfd5ea5e5160507838473a3a9383f9ee9a85b64a
Bram Swenson <bram@craniumisajar.com>
2010-10-02 12:23:12 -0500

    added haml support and converted templates

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,11 @@ module Storeresetter
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
 
     # JavaScript files you want as :defaults (application.js is always included).
     config.action_view.javascript_expansions[:defaults] = [
