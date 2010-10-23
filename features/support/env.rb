0df1aeb6be484af1d091efc6b7e4bf15e05b6f89
Bram Swenson <bram@craniumisajar.com>
2010-10-23 11:17:38 -0500

    set tmp path for capybara

--- features/support/env.rb
+++ features/support/env.rb
@@ -26,6 +26,7 @@ Capybara.default_selector = :css
 
 Capybara.default_driver = :envjs
 Capybara.javascript_driver = :envjs
+Capybara.save_and_open_page_path = '/tmp'
 # If you set this to false, any error raised from within your app will bubble 
 # up to your step definition and out to cucumber unless you catch it somewhere
 # on the way. You can make Rails rescue errors and render error pages on a
