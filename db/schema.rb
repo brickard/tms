669d6668df9f69def1fe7e2c8c03956ff6aa94db
Bram Swenson <bram@craniumisajar.com>
2010-10-23 11:50:01 -0500

    updated schema from migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101022014911) do
+ActiveRecord::Schema.define(:version => 20101023162445) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
@@ -129,6 +129,16 @@ ActiveRecord::Schema.define(:version => 20101022014911) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "number"
+  end
+
+  create_table "uniform_orders", :force => true do |t|
+    t.integer  "employee_id"
+    t.string   "shirt_size"
+    t.integer  "shirt_count"
+    t.string   "hat_size"
+    t.integer  "hat_count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "user_roles", :force => true do |t|
