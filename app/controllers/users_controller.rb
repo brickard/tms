29ecf600cdfb98a12bc777eac061c081ccbeb682
Bram Swenson <bram@craniumisajar.com>
2010-11-14 22:54:39 -0600

    updated user controller to redirect to index after create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to(@user, :notice => 'User was successfully created.') }
+        format.html { redirect_to(users_path, :notice => 'User was successfully created.') }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { render :action => "new" }
