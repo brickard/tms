115359b4724a801e8e278492a3a38f35239424cb
Bram Swenson <bram@craniumisajar.com>
2010-11-18 22:45:14 -0600

    updates from list

--- config/initializers/carmen.rb
+++ config/initializers/carmen.rb
@@ -1,5 +1,6 @@
 module Carmen
-  US_STATES = self.states('US').delete_if do |name, code| 
-    %w{ FM GU MH MP PW PR VI AA AE AP }.include?(code)
+  US_STATES =  [ [ '--Select--', nil ], ] + 
+    self.states('US').delete_if do |name, code| 
+      %w{ FM GU MH MP PW PR VI AA AE AP }.include?(code)
   end
 end
