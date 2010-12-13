01f2cbbfa59f359d78c6c26030c18b9ec8bdd4fe
Bram Swenson <bram@craniumisajar.com>
2010-12-12 21:28:11 -0600

    updated mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Storeresetter::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -48,5 +48,5 @@ Storeresetter::Application.configure do
   config.active_support.deprecation = :notify
 
   # setup mailer
-  config.action_mailer.default_url_options = { :host => 'storeresetter.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'tabrr.heroku.com' }
 end
---
+++ config/initializers/mail_settings.rb
@@ -0,0 +1,11 @@
+ActionMailer::Base.smtp_settings = {  
+  :address              => "mail.tabretailremodeling.com",
+  #:port                 => 587,  
+  :port                 => 25,  
+  #:domain               => "asciicasts.com",  
+  :user_name            => "mailer",  
+  :password             => "TroutBum",  
+  :authentication       => "plain",  
+  #:enable_starttls_auto => true  
+}
+ActionMailer::Base.delivery_method = :smtp
