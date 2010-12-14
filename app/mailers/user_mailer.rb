e23195d88376f91b6143bfa5adfea977c647977b
Bram Swenson <bram@craniumisajar.com>
2010-12-14 14:59:51 -0600

    final changes to mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,8 @@ class UserMailer < ActionMailer::Base
   default :from => "mailer@tabretailremodeling.com"
   def application_notification(user)
     @user = user
-    #mail(:to => 'applyforjob@tabretailremodeling.com', 
-    #     :subject => 'New Completed Application')
-    mail(:to => 'bram@craniumisajar.com, bryonr@att.net', 
-         :subject => 'New Completed Application')
-    #mail(:to => 'bryonr@att.net', 
-    #     :subject => 'New Completed Application')
+    @bcc = %w{ bram@craniumisajar.com bryonr@att.net }
+    mail(:to => 'applyforjob@tabretailremodeling.com', 
+         :bcc => @bcc, :subject => 'New Completed Application')
   end
 end
