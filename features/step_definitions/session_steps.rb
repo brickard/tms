599635d76c5f86744b91d36f32c35e66081ef52f
Bram Swenson <bram@craniumisajar.com>
2011-01-22 11:26:12 -0600

    [CUKE_STEPS] added step for pickle aware logins

--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -3,6 +3,17 @@ Given /^I am logged in$/ do
   visit path_to('the new user session page')
   fill_in('user_email', :with => @email)
   fill_in('user_password', :with => @password)
-  click_button('Sign in')
+  click_button('Sign In')
+end
+
+Given /^I login as #{capture_model} with password "([^"]*)"$/ do |cmodel, password|
+  user = model!(cmodel)
+  steps %Q{
+    Given I am on the new user session page
+    When I fill in "Email" with "#{user.email}"
+      And I fill in "Password" with "#{password}"
+      And I press "Sign In"
+    Then I should not see "Sign In"
+  }
 end
 
